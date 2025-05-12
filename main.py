13fa2bb8278bcbd11317207bbcf0ae8b348a84b1
Phillip Gachnang <phillip.gachnang@fhnw.ch>
2025-05-12 09:52:01 +0200

    added stars

---
+++ main.py
@@ -0,0 +1,5 @@
+from room import Room
+from hotel import Hotel
+
+if __name__ == "__main__":
+    print("Test")
---
+++ room.py
@@ -0,0 +1,6 @@
+from hotel import Hotel
+
+class Room:
+    
+    def __init__(self, hotel: Hotel):
+        self._hotel = hotel
